449f626c846b7913cd8305d4d844c2725038f7b1
surajpatwal22 <patwal0suraj@gmail.com>
2023-03-21 12:38:38 +0530

    currentwork

---
+++ ExpanceTracker/src/component/Balance.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Balance = () => {
+  return (
+    <div>
+     <h4>Your Balance</h4>
+        <h5 id='balance'>Rs 0.00</h5>
+
+    </div>
+  )
+}
+
+export default Balance
